29fe9f27e88ff8510bba9a5f165dc3c563ab5bc3
Megan <lawsonme@purdue.edu>
2025-02-13 16:18:52 -0500

    temp stash

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-#load
+import pickle
+import numpy as np
+import sys
+import time
+import matplotlib.pyplot as plt
+import spac
+from spac.visualization import histogram, cal_bin_num
 
+#load
+# Load the example data from dev_example.pickle
+file_path = "dev_example.pickle"
+with open(file_path, 'rb') as file:
+    adata = pickle.load(file)
+    
+file_path = 
 #feature
 def feature(): 
     pass
